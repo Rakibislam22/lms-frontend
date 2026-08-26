5f09f76efcb2bd2ade00cc4e80a1bc06eb2012af
Rakibislam22 <rakibislam13122018@gmail.com>
2026-08-27 01:42:21 +0600

    feat(api): add configured axios api client with auth interceptor

---
+++ src/lib/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337' });
+
+api.interceptors.request.use((config) => {
+    const isAuthEndpoint = config.url?.includes('/api/auth/local');
+    const token = Cookies.get('jwt');
+    if (token && !isAuthEndpoint) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
+export default api;
